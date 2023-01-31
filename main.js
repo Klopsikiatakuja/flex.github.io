90c8907dc995c3c05fa07f4fb164ca7c8f3cbc03
Klopsikiatakuja <117949444+Klopsikiatakuja@users.noreply.github.com>
2023-01-31 16:37:39 +0100

    1

--- main.js
+++
@@ -1,9 +0,0 @@
-const sidenav=()=>{
-    const menu= document.querySelector('.menu-icon');
-    const nav= document.querySelector('.nav');
-    menu.addEventListener('click',()=>{
-        menu.classList.toggle('menu-icon-actiive');
-        nav.classList.toggle('nav-active');
-    });
-}
-sidenav();
